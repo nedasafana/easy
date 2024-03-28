2e930bf2695c6a692f894efec466a97efe9a11f5
dasa <165267529+nedasafana@users.noreply.github.com>
2024-03-28 16:49:26 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-9fcdf097-3cc9-4fb4-917d-70282652feb2/)
 
 
 echo "$curl_output" >> Update.tsx
